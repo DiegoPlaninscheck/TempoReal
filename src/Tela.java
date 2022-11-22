bf99fb6a044a349390ff389207422d17e0c553ef
DiegoPlaninscheck <92130153+DiegoPlaninscheck@users.noreply.github.com>
2022-11-22 13:49:13 -0300

    Update Tela.java

--- src/Tela.java
+++ src/Tela.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -44,11 +45,46 @@ public class Tela extends JFrame implements Runnable {
 
             Long horarioMudanca = new Date().getTime();
 
+            gerarArquivo(horarioAtual, (horarioMudanca - horarioAtual));
             System.out.println(horarioMudanca - horarioAtual);
 
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
+
+    public void gerarArquivo(Long horaAtual, Long tempo) {
+        File arquivo = new File("C:\\Users\\Aluno\\Documents\\tempoReal.txt");
+        try {
+            if (!arquivo.exists()) {
+                //cria um arquivo (vazio)
+                arquivo.createNewFile();
+            }
+            //caso seja um diretório, é possível listar seus arquivos e diretórios
+            File[] arquivos = arquivo.listFiles();
+            //escreve no arquivo
+            FileWriter fw = new FileWriter(arquivo, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write("Tempo: " + horaAtual + " " + tempo);
+            bw.newLine();
+            bw.close();
+            fw.close();
+            //faz a leitura do arquivo
+            FileReader fr = new FileReader(arquivo);
+            BufferedReader br = new BufferedReader(fr);
+            //enquanto houver mais linhas
+            while (br.ready()) {
+                //lê a proxima linha
+                String linha = br.readLine();
+                //faz algo com a linha
+                System.out.println(linha);
+            }
+            br.close();
+            fr.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
     }
 
     @Override
